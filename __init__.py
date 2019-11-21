ae84cf454381efdb28839d94e59fb4fbc90f9d03
Alex Summer <alex.summer48@yahoo.com>
2019-11-20 21:53:05 -0500

    Wrapped up the features

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import os
+import functools
 
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -15,11 +17,36 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    from .models import db
+    from .models import db-, User, Note
 
     db.init_app(app)
     migrate = Migrate(app, db)
+    #Protecting our routes
+    #======================================
+    def require_login(view):
+        @functools.wraps(view)
+        def wrapped_view(**kwargs):
+            if not g.user:
+                return redirect(url_for('log_in'))
+            return view(**kwargs)
+        return wrapped_view
 
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+    
+    @app.before_request
+    def load_user():
+        user_id = session.get('user id')
+        if user_id:
+            g.user = User.query.get(user_id)
+        else:
+            g.user = None
+    #Setting up routes
+    #=======================================
+
+    #SIGN UP
+    #-----------------------------------------------
     @app.route('/sign up'methods = ('GET', 'POST'))
     def sign_up():
         if request.method == 'POST':
@@ -47,6 +74,8 @@ def create_app(test_config=None):
 
         return render_template('sign_up.html')
 
+    #LOG IN
+    #------------------------------------------------
     @app.route('/log_in')
     def log_in():
         if request.method == 'POST':
@@ -68,15 +97,85 @@ def create_app(test_config=None):
 
         return render_template('log_in.html')
 
+
+    #LOG OUT
+    #--------------------------------------------
     @app.route('/Log out', methods=('GET', 'DELETE'))
     def log_out():
         session.clear()
         flash('Successfully logged out!', 'success')
         return redirect(url_for('log_in'))
     
-    
+
+    #HOME
+    #----------------------------------------------------
     @app.route('/')
     def index():
-        return 'Index'
+        return redirect(url_for('note_index'))
+
+    #NOTES PAGE
+    #--------------------------------------------------------
+    @app.route('/notes')
+    @require_login
+    def note_index():
+        return render_template('note_index.html', notes=g.user.notes)
+    #NEW NOTES
+    #---------------------------------------------------------
+    @app.route('/notes/new', methods=('GET', 'POST'))
+    @require_login
+    def note_create()
+        if request.method == 'POST':
+            title = request.form['title']
+            body = request.form['body']
+            error = None
+
+            if not title:
+                error = 'Title is required.'
+
+            if not error:
+                note = Note(author=g.user, title=title, body=body)
+                db.session.add(note)
+                db.session.commit()
+                flash(f"Successfully created note: '{title}'", 'success')
+                return redirect(url_for('note_index'))
+
+            flash(error, 'error')
+
+        return render_template('note_create.html')
+    #EDIT NOTES
+    #----------------------------------------------------------
+    @app.route('/notes/<note_id>/edit', methods=('GET','POST', 'PATCH', 'PUT'))
+    @require_login
+    def note_update(note_id)
+        note = Note.query.filter_by(user_id=g.user.id, id=note_id).first_or_404()
+        if request.method in ['POST', 'PATCH', 'PUT']:
+            title = request.form['title']
+            body = request.body['title']
+            error = None
+
+            if not title:
+                error = 'Title is required.'
+
+            if not error:
+                note.title = title
+                note.body = body
+                db.session.add(note)
+                db.session.commit()
+                flash(f"Successfully updated note: '{title}'", 'success')
+                return redirect(url_for('note_index'))
+
+            flash(error, 'error')
+
+        return render_template('note_update.html',note=note)
+    #DELETE NOTES
+    #---------------------------------------------------------------------
+    @app.route('/notes/<note_id>/delete', methods=('GET', 'DELETE'))
+    @require_login
+    def note_delete(note_id):
+        note = Note.query.filter_by(user_id=g.user.id, id=note_id).first_or_404()
+        db.session.delete(note)
+        db.session.commit()
+        flash(f"Successfully deleted note: '{note.title}'", 'success')
+        return redirect(url_for('note_index'))
 
     return app
